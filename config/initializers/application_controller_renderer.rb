338d67d5ece7730aa97437fbdbe814622daffaa0
mai-hasunuma <mai.tomimura@gmail.com>
2019-08-04 08:17:49 +0000

    [Finish]3-9

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a1227759f241db1905cb9de0fa3a2c02adc46c8cb036487dad39ca962d46cf6551b2668cfffc32fa1812eb268e2eb44d0dff22fdd710b6f57e35827b4c63e668'
